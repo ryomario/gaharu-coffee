3d016ad800d4a02f624f996da0592400f1925a71
Mario <ardamomo3@gmail.com>
2023-09-25 14:54:46 +0000

    Add component: Footer
    Menambahkan footer dan menempatkan footer selalu terlihat dibawah

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,3 +13,11 @@
     @stack('breadcrumb')
     @yield('breadcrumb')
 @endsection
+
+@section('footer')
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 3.1.0
+    </div>
+    <strong>Copyright &copy; 2014-2023 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
+    reserved.
+@stop
